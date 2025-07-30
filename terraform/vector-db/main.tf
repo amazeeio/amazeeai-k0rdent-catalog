f2b90eb50c62bb40ceba083af3badea84df0a68e
Pippa H <pippa+github@pippah.net>
2025-07-30 09:03:12 +0200

    Update Crossplane Helm chart to version 0.1.4 and modify configurations
    - Bump chart version to 0.1.4 in Chart.yaml
    - Update module path for AWS Vector Database in aws-vector-db.yaml
    - Change Crossplane template version in crossplane-resources.yaml to 0.1.4
    - Adjust minCapacity parameter in vector-db-ir1.yaml
    - Add versions.tf file for Terraform provider requirements
    - Introduce new ServiceTemplate for Crossplane with updated version

--- terraform/vector-db/main.tf
+++ terraform/vector-db/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-      configuration_aliases = [aws]
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   alias  = "main"
